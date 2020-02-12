8d9d12b41d1c5f9047fb3c24b3ef82e908fdf313
Iftekhar Uddin <iftekhar@live.unc.edu>
2020-02-12 14:35:54 -0500

    finished up base code taken from the manual

--- main.py
+++ main.py
@@ -25,3 +25,28 @@ wheel_diameter = 56
 # For the Robot Educator this is 114 milimeters
 axle_track = 114
 
+# The DriveBase is composed of two motors, with a wheel on each motor.
+# The wheel_diameter and axle_track values are used to make the motors
+# move at the correct speed when you give a motor command.
+robot = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
+
+# The following loop makes the robot drive forward until it detects an
+# obstacle. Then it backs up and turns around. It keeps on doing this
+# until you stop the program.
+while True:
+    #Begin driving forward at 200 milimeters per second
+    robot.drive(200,0)
+
+    # Wait until an obstacle is detected. This is done by repeatedly
+    # doing nothing (waiting for 10 miliseconds) while the measured
+    # distance is still greater than 300 mm.
+    while obstacle_sensor.distance() > 300:
+        wait(10)
+
+    # Drive backward at 100 milimeters per second. Keep going for 2 seconds.
+    robot.drive_time(-100, 0, 2000)
+
+    # Turn around at 60 degrees per second, around the midpoint between
+    # the wheels. Keep going for 2 seconds.
+    robot.drive_time(0, 60, 2000)
+
